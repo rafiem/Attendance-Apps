669b7d6d7186bcf00d67048f1d78b62efe3738e0
rafiem <rafiemuhammad07@gmail.com>
2019-04-21 09:48:04 +0700

    add env.py for used in settings.py

--- presensi/settings.py
+++ presensi/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+from .env import secret_key, db_name, db_user, db_password ,db_host, db_port, db_engine
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tzl@9g58d#k$#^uy5(#@4y+)856u&hv@3v_jf7fkr**%aso^8)'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,12 +78,12 @@ AUTH_USER_MODEL = 'presensi_api.User'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'presensi',
-        'USER': 'django',
-        'PASSWORD': 'Djangowow_123',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': db_engine, 
+        'NAME': db_name,
+        'USER': db_user,
+        'PASSWORD': db_password,
+        'HOST': db_host,
+        'PORT': db_port,
     }
 }
 
