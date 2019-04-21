39b3381345e68fc47c52638968504c3e483f50e4
rafiem <rafiemuhammad07@gmail.com>
2019-04-21 09:49:17 +0700

    edit gitignore

--- presensi/env.py
+++
@@ -1,8 +0,0 @@
-
-secret_key    = 'tzl@9g58d#k$#^uy5(#@4y+)856u&hv@3v_jf7fkr**%aso^8)' 
-db_name       = 'presensi'
-db_user       = 'django'
-db_password   = 'Djangowow_123'
-db_host       = 'localhost'
-db_port       = '3306'
-db_engine     = 'django.db.backends.mysql'
